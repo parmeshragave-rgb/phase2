14a99ff770d8d146f994c5f1bb000e3ac47bcfe6
parmeshragave-rgb <parmeshragav.e@rently.com>
2025-11-06 15:13:55 +0530

    added logger middleware

--- src/Redux/HooksCounter.tsx
+++ src/Redux/HooksCounter.tsx
@@ -1,25 +1,28 @@
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material'
+import { Button, Card, CardActions, CardContent, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import {useDispatch, useSelector } from 'react-redux'
 import { increment,decrement,clear } from './CounterActions'
 import type { RootState,AppDispatch} from './Store'
+import { changeTheme } from './ThemeReducer'
 function HooksCounter() {
     const count=useSelector( (state :RootState) => state.counter.count)
     const theme=useSelector( (state :RootState) => state.theme.theme)
-
-    const dispatch=useDispatch<AppDispatch>()
+const dispatch=useDispatch<AppDispatch>()
   return (
    <>
+   <Toolbar/>
    <Card>
     <CardContent>
         <Typography variant='h3'>
-                {count}
+                {count} - {theme ? "Dark" : "Light"}
         </Typography>
     </CardContent>
     <CardActions>
         <Button onClick={() => dispatch(increment())}>Increment</Button>
         <Button onClick={() => dispatch(clear())}>Clear</Button>
         <Button onClick={() => dispatch(decrement())}>Decrement</Button>
+        <Button onClick={() => dispatch(changeTheme())}>toggle theme</Button>
+
         
     </CardActions>
    </Card>
--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,11 +1,12 @@
-import { createStore,combineReducers} from "redux";
+import { createStore,combineReducers,applyMiddleware} from "redux";
 import CounterReducer from "./CounterReducer";
 import ThemeReducer from "./ThemeReducer";
+import logger from "redux-logger";
 const RootReducer=combineReducers({
     counter:CounterReducer,
     theme:ThemeReducer
 })
-const store = createStore(RootReducer);
+const store = createStore(RootReducer,applyMiddleware(logger));
 export type RootState=  ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
 
